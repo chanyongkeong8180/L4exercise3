63df563a39a5909aa17edf856ca577b59fd14c8d
23048180-yongkeong <23048180@myrp.edu.sg>
2024-11-07 14:12:07 +0800

    Created Exercise 3A to 3E with flexbox stying.

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
-export default function App() {
+const Exercise = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.parent, {marginTop: 30}]}>
+      <View style={{backgroundColor: 'powderblue'}}>
+        <Text style={[styles.child]}>Child One</Text>
+      </View>
+
+      <View style={{backgroundColor: 'skyblue'}}>
+        <Text style={[styles.child]}>Child Two</Text>
+      </View>
+
+      <View style={{backgroundColor: 'steelblue'}}>
+        <Text style={[styles.child]}>Child Three</Text>
+      </View>
     </View>
   );
-}
+};
+export default Exercise;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  parent: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#f5fcff',
+      borderColor: '#0099aa',
+      borderWidth: 5,
+      justifyContent: 'space-around'
   },
+
+  child: {
+      borderWidth: 2,
+      textAlign: 'center',
+      fontSize: 24,
+  }
 });
